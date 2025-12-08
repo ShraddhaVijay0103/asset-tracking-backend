9441ce1d895e4b24a667f81f25d20eef2a4d3da5
Mohit-1510 <mohit@prosperinfotech.com>
2025-12-08 15:38:45 +0530

    Add full .NET Web API project to development branch

--- src/AssetTracking.Rfid.Api/obj/Debug/net8.0/AssetTracking.Rfid.Api.AssemblyInfo.cs
+++ src/AssetTracking.Rfid.Api/obj/Debug/net8.0/AssetTracking.Rfid.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetTracking.Rfid.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a979a43c74164b9e217b58513059b2d03b8fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetTracking.Rfid.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetTracking.Rfid.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/AssetTracking.Rfid.Domain/obj/Debug/net8.0/AssetTracking.Rfid.Domain.AssemblyInfo.cs
+++ src/AssetTracking.Rfid.Domain/obj/Debug/net8.0/AssetTracking.Rfid.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetTracking.Rfid.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a979a43c74164b9e217b58513059b2d03b8fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetTracking.Rfid.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetTracking.Rfid.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
